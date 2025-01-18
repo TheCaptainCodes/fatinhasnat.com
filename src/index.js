f84f4f31d547729f4eaabaeb8d5c5053d879a1d9
Captain Codes <official.captain.codes@gmail.com>
2025-01-18 23:16:22 +0600

    removed vercel speed insights

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SpeedInsights from '@vercel/speed-insights';
-
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>,
   document.getElementById('root'),
 );
