1d1589c19b43f14e7fd957843b040f2206c6f960
Captain Codes <official.captain.codes@gmail.com>
2025-01-18 23:01:18 +0600

    added Vercel Speed Insights

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
   document.getElementById('root'),
 );
